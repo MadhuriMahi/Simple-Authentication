a38d97bcc532563d5fbd56ab6d8ac0171cf430a7
MadhuriMahi <45658529+MadhuriMahi@users.noreply.github.com>
2021-11-19 18:11:16 +0530

    Update init.php

--- init.php
+++ init.php
@@ -3,7 +3,7 @@
 function goToAuthUrl()
 {
     $client_id = "1a6c8cb130adfd836956";
-    $redirect_url = "http://Simple-Authentication/callback.php";
+    $redirect_url = "http://localhost/Simple-Authentication/callback.php";
     if ($_SERVER['REQUEST_METHOD'] == 'GET') {
         $url = 'https://github.com/login/oauth/authorize?client_id='. $client_id. "&redirect_url=".$redirect_url."&scope=user";
         header("location: $url");
